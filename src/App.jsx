4bc43598a878de2342d405b0c3bfe11810398392
xdo13 <xdo13@naver.com>
2025-01-02 16:02:29 +0900

    쇼핑몰 네비게이션 바 및 메인사진

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { Button, Navbar, Container, Nav} from 'react-bootstrap'
+import bg from './img/bg.jpg'
+
+
+function App() {
+  return (
+    <div className='App'>
+      <>
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#features">Features</Nav.Link>
+              <Nav.Link href="#pricing">Pricing</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+        <div className='bg' style={{backgroundImage: 'url(' + bg + ')'}}></div>
+      </>
+    </div>
+   
+  );
+}
+
+export default App
